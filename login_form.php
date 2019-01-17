8515a2945ba0cd4a10a67215b18027df97cfa0a2
JulesKoster <juleskoster@hotmail.com>
2019-01-17 14:08:32 +0100

    added algemene voorwaarden modal, fixed resonsiveness

--- login_form.php
+++ login_form.php
@@ -23,6 +23,32 @@ include 'nav_header.php';
             </div>  
         </div>    -->
 
+        <!-- <form>
+        
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                <div class="col-sm-10">
+                <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-10">
+                <button type="submit" class="btn btn-primary">Sign in</button>
+                </div>
+            </div>
+            </form> -->
+        
+
+
+
+
+
     <!-- Login Form -->    
     <div class="container borderform">
         <div class="row justify-content-center">
@@ -30,7 +56,7 @@ include 'nav_header.php';
                 <form action="login.php" method="post">
                 <div class="form-group mb-0">
                     <label for="InputEmail1"></label>
-                    <div class="input-group">
+                    <div class="input-group col">
                         <div class="input-group-prepend"> 
                             <span class="input-group-text" id="inputGroupPrepend"><i class="fas fa-envelope"></i></span>   
                         </div> 
@@ -39,7 +65,7 @@ include 'nav_header.php';
                 </div>
                     <div class="form-group">
                         <label for="Inputpassword1"></label> 
-                        <div class="input-group">
+                        <div class="input-group col">
                             <div class="input-group-prepend"> 
                             <span class="input-group-text" id="inputGroupPrepend"><i class="fas fa-key"></i></span> 
                             </div>  
