ff6de80def09cfac9d89ee7f2f2425cc1d9f40f1
JulesKoster <juleskoster@hotmail.com>
2019-01-18 10:24:59 +0100

    included nav and footer on user.home needs fix, added form form for pw reset

--- forgot_password_test.php
+++ forgot_password_test.php
@@ -17,7 +17,7 @@
   <a href="#" class="btn btn-link btn-fill" data-target="#pwdModal" data-toggle="modal">Wachtwoord vergeten?</a>
 </div>
 
-<!--modal-->
+<!-- Modal Password Reset -->
 <div id="pwdModal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
   <div class="modal-dialog">
   <div class="modal-content">
@@ -59,7 +59,7 @@
   </div>
   </div>
 </div>
-
+<!-- Modal Password Reset -->
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
