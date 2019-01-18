aca1ee84d4a7b66b7d6c3ee3a80519d7ed295e11
Wilbert Schadee <wilbert_schadee_91@hotmail.com>
2019-01-18 09:45:54 +0100

    nav links made

--- nav_header.php
+++ nav_header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<?php session_start() ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -40,13 +41,13 @@
                         <a href="#">Klantenservice</a>
                     </li>
                     <li class="list-inline-item">
-                        <a href="#">Inloggen</a>
+                        <a href="login_form.php">Inloggen</a>
                     </li>
                     <li class="list-inline-item">
-                        <a href="#">Registreren</a>
+                        <a href="register_form.php">Registreren</a>
                     </li>
                     <li class="list-inline-item">
-                        <a href="#">Spelwijzer</a>
+                        <a href="game_select.php">Spelwijzer</a>
                     </li>
                     <li class="list-inline-item">
                         <a href="#" class="wishlist">Verlanglijst</a>
@@ -65,7 +66,8 @@
                       <i class='fas fa-shopping-cart'></i></a>
                     </li>
                     <li class="list-inline-item">
-                    <span class="count">0</span>
+                    <span class="count"><?php echo count($_SESSION['cart']); ?>
+                    </span>
                     </li>
                   </ul>
             </div>
@@ -110,15 +112,6 @@
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">overige</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"></a>
               </li>
             </ul>
           </div>
