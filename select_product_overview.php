e3de28ae425086038acf791b19988ab0cdfc0e5a
Wilbert Schadee <wilbert_schadee_91@hotmail.com>
2019-01-18 11:02:20 +0100

    added some images

--- select_product_overview.php
+++ select_product_overview.php
@@ -1,6 +1,23 @@
 <?php 
     include "connect.php";
-        $sql = "SELECT * FROM products";
+
+    if (!empty($_GET['classic'])) {
+        $classic = $_GET['classic'];
+        $sql = "SELECT * FROM products WHERE product_classic = '$classic'";
+    }else if(!empty($_GET['category'])){
+
+        $category = $_GET['category'];
+        $sql = "SELECT * FROM products WHERE product_category = '$category'";
+    }else if (!empty($_GET['travelversion'])){
+        $travelversion = $_GET['travelversion'];
+        $sql = "SELECT * FROM products WHERE product_travelversion = '$travelversion'";
+    }else if (!empty($_GET['kickstarter'])){
+     $kickstarter = $_GET['kickstarter'];
+     $sql = "SELECT * FROM products WHERE product_kickstarter = '$kickstarter'";
+    }else{
+         $sql = "SELECT * FROM products";
+    }
+       
         $data = $pdo->query($sql);
         $divCounter = 0;
 
@@ -9,7 +26,7 @@
             $product_desc = $row['product_desc'];
             $dataObject[$divCounter] = $row;
             $htmlOutput[$divCounter]  = '';
-            $htmlOutput[$divCounter]  = '<a href="single_product.php?product_id=' . $divCounter . '">';
+            $htmlOutput[$divCounter]  = '<a href="single_product.php?product_id=' . $row['product_id'] . '">';
             $htmlOutput[$divCounter] .= '<div class="card" style="width: 18rem; height: 30em;">';
             $htmlOutput[$divCounter] .= '<img class="card-img-top img-thumbnail" style="height: 15em;" src="img/products/' . $row['product_img'] . '" alt="Card image cap">';
             $htmlOutput[$divCounter] .= '<div class="card-body">';
