6f087d9748199e08615d4f6248234eea3de0e0be
Jira Verkooijen <jiver@hotmail.nl>
2019-01-29 10:10:20 +0100

    shadow buttons navbar

--- footer.php
+++ footer.php
@@ -4,17 +4,17 @@
         <div class='row d-flex justify-content-around'>
             <!-- <div class='col-lg-2 mt-5 footerColumn'></div> -->
             <div class='col-lg-2 mt-5 footerColumn'>
-                <h4>Klantenservice</h4>
+                <H5>KLANTENSERVICE</H5>
                 <a href='tel:076-1234567'><p><i class="fas fa-phone-square footerIcon"></a></i>&nbsp;&nbsp;&nbsp;<a href='tel:076-1234567'>076-1234567</a></p>
                 <a href='mailto:email@email.com'><p><i class="fas fa-envelope-square footerIcon"></a></i>&nbsp;&nbsp;&nbsp;<a href='mailto:email@email.com'>email@email.com</p></a>
             </div>
             <div class='col-lg-2 mt-5 footerColumn'>
-                <h4>Nieuwsbrief ontvangen</h4>
+                <H5>NIEUWSBRIEF</H5>
                 <input type="text" class="form-control" id="validationDefault01" placeholder="Vul email-adres in">
                 <input class="btn btn-secondary mt-1" type="button" value="Inschrijven">
             </div>
             <div class='col-lg-2 mt-5 footerColumn'>
-                <h4>Contactgegevens</h4>
+                <H5>CONTACTGEGEVENS</H5>
                 <p>Game ON!</p>
                 <p>Bordspellenweg 1</p>
                 <p>1111GO Breda</p>
@@ -23,7 +23,7 @@
                 <a href='https://github.com/wilbertschadee/boardgames/' target='_blank'><i class="fab fa-github-square footerIcon" id='ghFont'></i></a>&nbsp;
             </div>
             <div class='col-lg-2 mt-5 footerColumn'>
-                <h4>Direct naar </h4>
+                <H5>DIRECT NAAR </H5>
                 <p>Spelwijzer</p>
                 <p>Mijn persoonlijke pagina</p>
                 <p>Chatbox</p>
