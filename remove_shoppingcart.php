efbe98f5d524cab3c31a89cbd6a925d9192116c0
KaHongTam <kahongtam@hotmail.com>
2019-01-31 15:30:12 +0100

    side scrolling fixed

--- remove_shoppingcart.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if(!isset($_SESSION)) {
-        session_start(); 
-    }
-    $item = $_GET['item'];
-    include 'functions_shoppingcart.php';
-    $_SESSION['cart'] = removeByIndex($item, $_SESSION['cart']);
-    header('Location: view_shoppingcart.php'); 
-?>
