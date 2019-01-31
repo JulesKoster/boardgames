5104ccd82b62f6478e740140b558774af941f0b6
JulesKoster <juleskoster@hotmail.com>
2019-01-31 11:17:43 +0100

    started cleaning login_form

--- login_form.php
+++ login_form.php
@@ -13,39 +13,7 @@
         include 'nav_header.php';  
         include 'login.php';              
         ?>
-        <!-- <div class="form-wrapper">
-            <div class="contact-form">
-            <h1>Login</h1>
-            <form action="login.php" method="post">
-            <label for="useremail">Username</label>
-            <input type="text" id="userEmail" name="userEmail" placeholder="Enter your E-mail"><br>
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="Enter your password"><br>
-            <button type="submit" class="login" name="login" value="Login">Login</button>
-            </form>
-            </div>  
-        </div>    -->
-
-        <!-- <form>
-        
-            <div class="form-group row">
-                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                <div class="col-sm-10">
-                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-                <div class="col-sm-10">
-                <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-                </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-sm-10">
-                <button type="submit" class="btn btn-primary">Sign in</button>
-                </div>
-            </div>
-            </form> -->
+    </head>    
 
     <!-- Login Form -->    
     <div class="container borderform my-5">
@@ -79,9 +47,8 @@
                         // }
                         // if ($showPasswordMessage) {
                         //    echo ($passwordMessage);
-                        // }
+                        // }                       
                         ?>
-                        
                         <a href="register_form.php"><button type="button" class="btn btn-outline-primary btn-lg btn-block">Nog geen account? Maak hier een account aan!</button>
                       </a>
                     
