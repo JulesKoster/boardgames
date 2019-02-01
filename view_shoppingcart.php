25614f4d6a5a681e132449a2e91be18cf52c31fd
KaHongTam <kahongtam@hotmail.com>
2019-02-01 13:01:13 +0100

    bug fixing change amount

--- view_shoppingcart.php
+++ view_shoppingcart.php
@@ -66,5 +66,6 @@
                 include 'footer.php';
             ?>
             <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.6.3/css/all.css' integrity='sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/' crossorigin='anonymous'>
+            <script src="js/calculation.js"></script>
     </body>
 </html>
